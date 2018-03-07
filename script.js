cd826b1ffee9b11598b69f27a285f268de1b9a79
pgakk <35960144+pgakk@users.noreply.github.com>
2018-03-07 16:07:16 -0500

    Code refactor

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-var css_gradient = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.querySelector("#gradient");
-var button = document.querySelector("button");
+const css_gradient = document.querySelector("h3");
+const [color1, color2] = document.querySelectorAll(".color");
+const body = document.querySelector("#gradient");
+const button = document.querySelector("button");
 
 function setBackground() {
-    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
     css_gradient.textContent = body.style.background;
 }
 
 function setBackgroundGradient(color1, color2) {
-    body.style.background = "linear-gradient(to right, " + color1 + ", " + color2 + ")";
+    body.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
     css_gradient.textContent = body.style.background;
 }
 
 function randomColorGenerator() {
-    var color1 = generateRandomColors();
-    var color2 = generateRandomColors();
+    let color1 = generateRandomColors();
+    let color2 = generateRandomColors();
 
     setBackgroundGradient(color1, color2);
 }
@@ -26,8 +25,8 @@ function randomColorGenerator() {
 // }
 
 function generateRandomColors() {
-    var rgb = [];
-    for(var i = 0; i < 3; i++){
+    let rgb = [];
+    for(let i = 0; i < 3; i++){
         rgb[i] = Math.floor(Math.random() * 256);
     }
     // return "rgb(" + generateRandom() + ", " + generateRandom() + ", " + generateRandom()+ ")";
